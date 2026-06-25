c5196fc45235ed3ea4ee16b5b5218cab950cb748
Muhammad Al Faris <muhammadalfaris522@gmail.com>
2026-06-25 10:29:28 +0700

    fix: add api v1 route prefix

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\Api\ContractController;
 use App\Http\Controllers\Api\SsoController;
 
 
-Route::get('/contracts', [ContractController::class, 'index']);
-Route::post('/contracts', [ContractController::class, 'store']);
-Route::get('/contracts/{id}', [ContractController::class, 'show']);
-Route::put('/contracts/{id}', [ContractController::class, 'update']);
-Route::delete('/contracts/{id}', [ContractController::class, 'destroy']);
+Route::prefix('v1')->group(function () {
+    Route::post('/auth/login', [SsoController::class, 'login']);
 
-Route::post('/sso/login', [SsoController::class, 'login']);
-Route::post('/contracts/{id}/approve', [ContractController::class, 'approve']);
+    Route::get('/contracts', [ContractController::class, 'index']);
+    Route::get('/contracts/{id}', [ContractController::class, 'show']);
+    Route::post('/contracts', [ContractController::class, 'store']);
+    Route::put('/contracts/{id}', [ContractController::class, 'update']);
+    Route::delete('/contracts/{id}', [ContractController::class, 'destroy']);
+
+    Route::post('/contracts/{id}/approve', [ContractController::class, 'approve']);
+});
